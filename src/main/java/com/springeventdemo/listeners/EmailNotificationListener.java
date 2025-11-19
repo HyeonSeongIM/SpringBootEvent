47fa0b985c7d8578c080c27842d2049ec2da1972
HyeonSeong <gus8131@naver.com>
2025-11-19 18:00:35 +0900

    비동기 이벤트 처리

--- src/main/java/com/springeventdemo/listeners/EmailNotificationListener.java
+++ src/main/java/com/springeventdemo/listeners/EmailNotificationListener.java
@@ -2,15 +2,17 @@ package com.springeventdemo.listeners;
 
 import com.springeventdemo.events.OrderCreatedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailNotificationListener {
 
     @EventListener
-    @Order(2)
-    public void handleOrderCreatedAndSendEmail(OrderCreatedEvent event) {
+    @Async
+    public void handleOrderCreatedAndSendEmail(OrderCreatedEvent event) throws InterruptedException {
         System.out.println("Email Notification Listener received an email notification : " + event.getOrderId());
+        Thread.sleep(3000);
+        System.out.println("Email sent : " + event.getOrderId());
     }
 }
--- src/main/java/com/springeventdemo/listeners/LogOrderDetailsListener.java
+++ src/main/java/com/springeventdemo/listeners/LogOrderDetailsListener.java
@@ -2,15 +2,17 @@ package com.springeventdemo.listeners;
 
 import com.springeventdemo.events.OrderCreatedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LogOrderDetailsListener {
 
     @EventListener
-    @Order(1)
-    public void logOrderCreated(OrderCreatedEvent event) {
+    @Async
+    public void logOrderCreated(OrderCreatedEvent event) throws InterruptedException {
         System.out.println("Order placed orderId : " + event.getOrderId());
+        Thread.sleep(1000);
+        System.out.println("Order placed Successfully : " + event.getOrderId());
     }
 }
