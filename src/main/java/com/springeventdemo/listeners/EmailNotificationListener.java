3cdf5d75e5a36f0162843b9423c608273b3f5dc2
HyeonSeong <gus8131@naver.com>
2025-11-19 17:52:32 +0900

    이벤트간 순서 생성

--- src/main/java/com/springeventdemo/listeners/EmailNotificationListener.java
+++ src/main/java/com/springeventdemo/listeners/EmailNotificationListener.java
@@ -2,12 +2,14 @@ package com.springeventdemo.listeners;
 
 import com.springeventdemo.events.OrderCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailNotificationListener {
 
     @EventListener
+    @Order(2)
     public void handleOrderCreatedAndSendEmail(OrderCreatedEvent event) {
         System.out.println("Email Notification Listener received an email notification : " + event.getOrderId());
     }
